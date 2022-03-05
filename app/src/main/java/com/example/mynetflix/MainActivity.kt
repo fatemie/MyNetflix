cb118ccebda9940547a3ce2b1457feb86984c6c1
fateme <fs.fatemi@chmail.ir>
2022-03-05 10:21:24 +0330

    menu and blank fragments added

---
+++ app/src/main/java/com/example/mynetflix/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.mynetflix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.Menu
+import android.view.MenuInflater
+
+class MainActivity() : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+}
